af6e3c8be49bc7b0878e8741bfc7048859ad7db5
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2025-12-18 00:03:45 +0600

    Add files via upload

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    //Added the navbar
+   <div className='page_container'>
+     <div className="pages">
+            <ul>
+                <li><i class="fa-solid fa-house"></i><a href="/">Home</a></li>
+                <li><i class="fa-solid fa-store"></i><a href="/store">Store</a></li>
+                <li><i class="fa-solid fa-table-list"></i><a href="/categories">Categories</a></li>
+            </ul>
+        </div>
+   </div>
+  )
+}
+
+export default Navbar
