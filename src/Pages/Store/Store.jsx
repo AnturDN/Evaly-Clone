f585154720b7e4b1a5c0f7141260381ec073b551
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2025-12-26 01:51:28 +0600

    Add files via upload

---
+++ src/Pages/Store/Store.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../Component/Header/Header'
+import Navbar from '../../Component/Navbar/Navbar'
+import Card5_map from '../../Component/Store_Cards/Card5_map'
+import Footer from '../../Component/Footer/Footer'
+
+const Store = () => {
+  return (
+    <div>
+        
+        <Header/>
+        <Navbar/>
+        <Card5_map/>
+        <Footer/>
+        
+    </div>
+  )
+}
+
+export default Store
