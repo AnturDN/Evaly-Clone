40946d2782a757517cf508dc94eb80637fe09644
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-01-05 21:47:37 +0600

    Add files via upload

---
+++ src/Pages/Categories/Categories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Component/Header/Header'
+import Navbar from '../../Component/Navbar/Navbar'
+
+const Categories = () => {
+  return (
+    <div>
+        <Header/>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Categories
